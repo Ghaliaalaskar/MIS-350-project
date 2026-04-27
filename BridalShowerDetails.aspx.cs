497d5328ffad24805e336e15a292af05128a3fa0
Bazah-az <445200524@student.ksu.edu.sa>
2026-04-27 14:04:32 +0300

    bridal shower details after login

---
+++ BridalShowerDetails.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MIS_350_project
+{
+    public partial class BridalShowerDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBook_Click(object sender, EventArgs e)
+        {
+            Session["SelectedTheme"] = "Bridal Shower";
+            
+            Response.Redirect("BookingSelection.aspx");
+        }
+    }
+}
